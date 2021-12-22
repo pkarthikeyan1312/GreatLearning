72171c4011844c33d1b7d5e9611a95816d4dc38a
pkarthikeyan1312 <96464279+pkarthikeyan1312@users.noreply.github.com>
2021-12-22 11:38:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class Helloworld{
-  public static void main(){
-    System.out.println("Helloworld");
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
